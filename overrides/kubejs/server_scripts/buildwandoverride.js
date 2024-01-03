d059ec4a14ea488cfb17fab5c8ceb256467ddab6
Tarisznyarak <144532783+Tarisznyarak@users.noreply.github.com>
2024-01-03 18:20:16 +0100

    szia

---
+++ overrides/kubejs/server_scripts/buildwandoverride.js
@@ -0,0 +1,10 @@
+event.shaped('structurize:sceptergold:', [// arg 1: output
+    '  B', 
+    ' A ', // arg 2: the shape (array of strings)
+    'A  '  
+  ], {
+    A: 'minecraft:stick', 
+    B: 'minecraft:stone'  //arg 3: the mapping object   
+  }
+)
+
